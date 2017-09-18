c620f388bb30d07a972509c9915e8a91241a9cff
Sen <zenzosen@gmail.com>
2017-09-17 22:34:07 -0400

    Update P1_number_input.py

--- P1_number_input.py
+++ P1_number_input.py
@@ -1,12 +1,6 @@
 # first program in python
-# input two numbers, add them together, print them out
-# wfp, 9/1/07; rje, 5/5/14
 
-
-# defining the answer
-answer = 194
-
-#start game
+#start game with puzzle stored in variable p
 p =  '\n\
 I am a three-digit number.\n\
 My tens digit is 5 more than my ones digit.\n\
@@ -14,16 +8,34 @@ My hundreds digit is 8 less than my tens digit.\n\
 What number am I?\n\
 '
 
-print(p)
-
-num_str1 = input('Please enter an integer: ')
-int_var = int(num_str1)
-
-while (int_var != answer):
-    print('try again')
-    print(p)
-    num_str1 = input('Please enter an integer (hundreds digit): ')
+# defining the answer of the puzzle
+answer = 194
+   
+ 
+def main():
+    'a guessing game that continues untill responded gives the correct answer'
     
-input('You WIN!!')
+    print(p)
+    
+    #this variable stores the respondends guess
+    num_str1 = input('Please enter an integer: ')
+    
+    # loop until the right answer is given
+    while num_str1 != answer:
+
+        if num_str1 > answer:
+            print('try again, answer is lower')  
+            print(p)
+            num_str1 = int(input('Please enter an integer: '))
+            
+        elif num_str1 < answer:
+            print('try again, the answer is higher')
+            print(p)
+            num_str1 = int(input('Please enter an integer: '))
+            
+    print('You WIN!!')        
+
+        
+main()
 
     
