cdc52f7408227b4074b80fd2ea494bbf4c0688fe
Sen <zenzosen@gmail.com>
2017-09-19 17:18:21 -0400

    Update P1_number_input.py

--- P1_number_input.py
+++ P1_number_input.py
@@ -1,29 +1,51 @@
-# first program in python
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author: Sen Varghese
+CUS 602
+HW1
+Objective: a guessing game that continues untill responded gives the correct answer
+"""
 
 #start game with puzzle stored in variable p
 p =  '\n\
-I am a three-digit number.\n\
-My tens digit is 5 more than my ones digit.\n\
-My hundreds digit is 8 less than my tens digit.\n\
-What number am I?\n\
+Enter a three-digit number whose tens digit \n\
+is at least as big as its units digit and \n\
+the tens digit is also at least as big as its hundreds digit.\n\
 '
 
 # defining the answer of the puzzle
-answer = 194
-   
- 
+import random
+H = random.randint(1,9) #Hundreds unit of the answer
+U = random.randint(1,9) #Units unit of the answer
+T = random.randint(max(H,U),9) #Tens unit of the answer
+answer = H*100 + T*10 + U #This the unique generated answer 
+#Question 5 does the puzzle you generate have multiple answers?: No multiple answers per session
+
+
+#The main function
 def main():
     'a guessing game that continues untill responded gives the correct answer'
-    
     print(p)
     
     #this variable stores the respondends guess
-    num_str1 = input('Please enter an integer: ')
+	# Question1. What happens when the user enters a letter instead of a number at the prompt?
+    try:
+        num_str1 = int(input('Please enter an integer: '))
+    except:
+        print('Dude! please enter a number!')
+        main()
     
     # loop until the right answer is given
     while num_str1 != answer:
-
-        if num_str1 > answer:
+        #adjustment for responded not entering a 3digit number (Question 3 & 4)
+        if num_str1 < 100 or num_str1 > 999:
+            print ('Please enter a 3 digit number\n\
+                   ')
+            print(p)
+            num_str1 = int(input('Please enter an integer: '))
+        
+        elif num_str1 > answer:
             print('try again, answer is lower')  
             print(p)
             num_str1 = int(input('Please enter an integer: '))
@@ -33,9 +55,6 @@ def main():
             print(p)
             num_str1 = int(input('Please enter an integer: '))
             
-    print('You WIN!!')        
-
+    print('You WIN!!')       
         
 main()
-
-    
